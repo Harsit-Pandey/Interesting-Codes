1866721193e6c1803e873eefc9bc651ee711e822
Harsit-Pandey <102853612+Harsit-Pandey@users.noreply.github.com>
2022-04-02 23:17:32 +0530

    Add files via upload
    Saving a Desired Fibonacci Series (0,1,1,2,3,5,8...) in a list and printing it.

---
+++ fibonacci.py
@@ -0,0 +1,21 @@
+def ser(n,c=[]):
+    if(n==0 or n==1): 
+        #print(n,end=",")
+        return n
+    t=ser(n-2)+ser(n-1,c)
+    c.append(t)
+    #print(t,end=",")
+    return(t)
+n=int(input("Enter a natural number-:"))
+if(n<0):
+    print("Negetive number found.")
+    exit(0)
+a=[0,1]
+if n==0:
+    print("0 is Not a Valid Value.")
+    exit(0)
+if n==1:
+        print(a[0],end='.')
+else: 
+    ser(n-1,a)
+    print(a)
