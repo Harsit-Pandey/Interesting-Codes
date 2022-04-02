1d4718e7c1e3dae33fe4b197c441ddc4db8179f4
Harsit-Pandey <102853612+Harsit-Pandey@users.noreply.github.com>
2022-04-02 23:31:58 +0530

    Different Combination From a Given Number
    Different Combination which can be formed from an existing number like (For 12, Combination are -> 12 & 21). (For 123 -> 123, 132, 312, 321, 231, 213)

---
+++ permutation.py
@@ -0,0 +1,37 @@
+def fun(a,b,n):
+    t=[]
+    if n>=0:
+        for i in range(n):
+                fun(a,b,n-1)
+                temp=b[n]
+                b[n]=b[n-1]
+                b[n-1]=temp
+                temp=1
+                x=0
+                for i in range(len(b)):
+                    x+=temp*b[i]
+                    temp=temp*10
+                count=0
+                for i in a:
+                    count=0
+                    if x==i:
+                        count+=1
+                        break
+                if count==0: 
+                    a.append(x)
+                t=b.copy()
+        fun(a,t,n-1)
+a=int(input("Enter a Number (Non-Negitive Number) -:"))
+b=[]
+c=[]
+i=a
+count=0
+if a<0:
+    print("Error Occured - Negitve Number Found.")
+    exit(0)
+while(i!=0):
+    b.append(i%10)
+    i//=10
+c.append(a)
+fun(c,b,len(b)-1)
+print("\nAll Possible Combination From Given Number Are -:",c)
